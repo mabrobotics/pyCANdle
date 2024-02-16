4f3f638396ad66ad7f77f428f1ad5a5480a02d24
klonyyy <piwasilewski@interia.pl>
2024-02-16 09:23:43 +0100

    fix: fixed compilation error due to missing header

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ readme = open("README.md").read()
 
 setup(
     name="pyCandleMAB",
-    version="1.5.0",
+    version="1.5.1",
     author="Piotr Wasilewski",
     author_email="support.md80@mabrobotics.pl",
     description="Python package for controlling MD80-based actuators",
